0b2b2bb89a14f9b01d12b8ee30dfb5624636f365
RamyaLakshmiKS <ramyalakshmi.ks@gmail.com>
2024-11-26 19:18:47 -0500

    change run app name

--- run_app.py
+++ run_app.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def main():
-    subprocess.run(["streamlit", "run", "src/bully_bot/app.py"])
+    subprocess.run(["streamlit", "run", "src/cowculator/app.py"])
 
 if __name__ == "__main__":
     main()
