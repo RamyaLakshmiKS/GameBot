b47ff76805a82a37c5d49057fdf8b607c08bb7aa
RamyaLakshmiKS <ramyalakshmi.ks@gmail.com>
2024-11-26 20:37:31 -0500

    change repo name and folder names

--- run_app.py
+++ run_app.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def main():
-    subprocess.run(["streamlit", "run", "src/cowculator/app.py"])
+    subprocess.run(["streamlit", "run", "src/gamebot/app.py"])
 
 if __name__ == "__main__":
     main()
